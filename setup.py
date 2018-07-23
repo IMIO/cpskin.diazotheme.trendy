1de4aebb29980caf63bae782d1db17ebec753770
maud gennart <maud.gennart@imio.be>
2018-07-23 13:27:17 +0200

    Preparing release 0.3.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.trendy package."""
 
-version = '0.3.4.dev0'
+version = '0.3.4'
 
 from setuptools import find_packages
 from setuptools import setup
